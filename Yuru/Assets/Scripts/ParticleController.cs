68f2d94ac5f326743aa5ec5b040f4a2453a8cfa4
KMC0319 <31245129+KMC0319@users.noreply.github.com>
2018-09-30 18:59:49 +0900

    add Medjed anim and AttackEffect
    Jumping Falling

---
+++ Yuru/Assets/Scripts/ParticleController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleController : MonoBehaviour {
+	private ParticleSystem myParticle;
+	void Awake () {
+		myParticle = GetComponent<ParticleSystem>();
+	}
+
+	public void Playparticle(Color particleColor, float playTime) {
+		ParticleSystem.MinMaxGradient color = new ParticleSystem.MinMaxGradient {mode = ParticleSystemGradientMode.Color, color = particleColor};
+		ParticleSystem.MainModule main = myParticle.main;
+		main.startColor = color;
+		myParticle.Play();
+		Invoke(nameof(StopParticle), playTime);
+	}
+
+	private void StopParticle() {
+		myParticle.Stop();
+	}
+}
