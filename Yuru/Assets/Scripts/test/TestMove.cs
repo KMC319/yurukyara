a958fd6a5ff26959e8750d8d40e95e7e0d18e677
KMC0319 <31245129+KMC0319@users.noreply.github.com>
2018-09-30 19:52:52 +0900

    effect update

--- Yuru/Assets/Scripts/test/TestMove.cs
+++ Yuru/Assets/Scripts/test/TestMove.cs
@@ -13,19 +13,20 @@ public class TestMove : MonoBehaviour {
     void Start() {
         rigid = GetComponent<Rigidbody>();
         lookTarget = transform.Find("LookTarget");
-        anim = GetComponent<Animator>();
+        anim = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
     void Update() {
-        
+        Move3D();
     }
 
     public void Move3D() {
         var z = lookTarget.forward * Input.GetAxis("Vertical") * speed;
         var x = lookTarget.right * Input.GetAxis("Horizontal") * speed;
         if (Vector3.Magnitude(z + x) == 0) {
-            Move3D(0);
+            transform.rotation = lookTarget.rotation;
+            anim.SetBool("Runflg", false);
             return;
         }
         rigid.velocity = new Vector3(0, rigid.velocity.y, 0) + z + x;
@@ -33,12 +34,6 @@ public class TestMove : MonoBehaviour {
         anim.SetBool("Runflg", true);
     }
 
-    public void Move3D(float zero) {
-        rigid.velocity = new Vector3(0, rigid.velocity.y, 0);
-        rigid.angularVelocity = Vector3.zero;
-        transform.rotation = lookTarget.rotation;
-        anim.SetBool("Runflg", false);
-    }
     public void Move2D() {
         var z = lookTarget.forward * Input.GetAxis("Horizontal") * speed;
         if (Vector3.Magnitude(z) == 0) {
