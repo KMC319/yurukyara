78f573032ac806d737f2cc8217f9e4b01e8b9d40
KMC0319 <31245129+KMC0319@users.noreply.github.com>
2018-09-30 19:28:41 +0900

    cam
    camcam

--- Yuru/Assets/Scripts/test/TestMove.cs
+++ Yuru/Assets/Scripts/test/TestMove.cs
@@ -13,46 +13,34 @@ public class TestMove : MonoBehaviour {
     void Start() {
         rigid = GetComponent<Rigidbody>();
         lookTarget = transform.Find("LookTarget");
-        anim = GetComponent<Animator>();
+        anim = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
     void Update() {
-        
+        Move3D();
     }
 
     public void Move3D() {
         var z = lookTarget.forward * Input.GetAxis("Vertical") * speed;
         var x = lookTarget.right * Input.GetAxis("Horizontal") * speed;
         if (Vector3.Magnitude(z + x) == 0) {
-            Move3D(0);
+            transform.rotation = lookTarget.rotation;
+            anim.SetBool("Runflg", false);
             return;
         }
         rigid.velocity = new Vector3(0, rigid.velocity.y, 0) + z + x;
         transform.LookAt(transform.position + rigid.velocity);
         anim.SetBool("Runflg", true);
     }
-
-    public void Move3D(float zero) {
-        rigid.velocity = new Vector3(0, rigid.velocity.y, 0);
-        rigid.angularVelocity = Vector3.zero;
-        transform.rotation = lookTarget.rotation;
-        anim.SetBool("Runflg", false);
-    }
     public void Move2D() {
         var z = lookTarget.forward * Input.GetAxis("Horizontal") * speed;
         if (Vector3.Magnitude(z) == 0) {
-            Move2D(0);
+            anim.SetBool("Runflg", false);
             return;
         }
         rigid.velocity = new Vector3(0, rigid.velocity.y, 0) + z;
         transform.LookAt(transform.position + rigid.velocity);
         anim.SetBool("Runflg", true);
-    }
-    public void Move2D(float zero) {
-        rigid.velocity = new Vector3(0, rigid.velocity.y, 0);
-        rigid.angularVelocity = Vector3.zero;
-        transform.rotation = lookTarget.rotation;
-        anim.SetBool("Runflg", false);
     }
 }
