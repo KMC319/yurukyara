13295a80ec69e8bb2c94d58dbca236e8bc50e539
domasyake <lehmboden.doma@gmail.com>
2018-10-16 13:24:40 +0900

    create shot tool

---
+++ Yuru/Assets/Scripts/Battles/Attack/ShotTool.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UniRx;
+
+namespace Battles.Attack{
+	public class ShotTool : AttackTool{
+		[SerializeField] private Bullet bullet;
+
+		private void Start(){
+			bullet.HitStream.Subscribe(n => hitStream.OnNext(n));
+		}
+
+		public override void On(){
+			Instantiate(bullet.gameObject,transform.position,transform.rotation);
+		}
+
+		public override void Off(){
+			
+		}
+	}
+}
