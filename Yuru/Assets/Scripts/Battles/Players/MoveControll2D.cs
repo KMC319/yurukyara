540264d1d780061ba204df07d5198b8965508ea7
KMC0319 <31245129+KMC0319@users.noreply.github.com>
2018-11-21 01:19:50 +0900

    update 2dmove

--- Yuru/Assets/Scripts/Battles/Players/MoveControll2D.cs
+++ Yuru/Assets/Scripts/Battles/Players/MoveControll2D.cs
@@ -2,14 +2,21 @@
 using UnityEngine;
 
 namespace Battles.Players{
-	public class MoveControll2D :MoveCotroll{
+	public class MoveControll2D :MoveCotroll {
+		private GameObject vcam2d;
+
+		protected override void Start() {
+			base.Start();
+			vcam2d = GameObject.Find("vcam 2d");
+		}
+
 		public override void Move(){
 			if (Math.Abs(HorizontalMovement) < 0.01f ){
 				Stop();
 				return;
 			}
 
-			var z = lookTarget.forward * HorizontalMovement * speed;
+			var z = vcam2d.transform.right * HorizontalMovement * speed;
 			rigid.velocity = new Vector3(0, rigid.velocity.y, 0) + z ;
 			transform.LookAt(transform.position + z );
 			if (!InJumping&&jumpAble){
