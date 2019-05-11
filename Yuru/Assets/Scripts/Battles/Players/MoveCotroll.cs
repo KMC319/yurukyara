e7dc940b078f3f707d24fe511386bdc5bec215bf
KMC0319 <31245129+KMC0319@users.noreply.github.com>
2019-05-11 13:12:01 +0900

    bug fix

--- Yuru/Assets/Scripts/Battles/Players/MoveCotroll.cs
+++ Yuru/Assets/Scripts/Battles/Players/MoveCotroll.cs
@@ -5,137 +5,145 @@ using doma;
 using UniRx;
 using UnityEngine;
 
-namespace Battles.Players{
-	public abstract class MoveCotroll:MonoBehaviour,IPlayerCancelProcess{
-		[SerializeField] protected float speed;
-		[SerializeField] private float jumpPower;
-		[SerializeField] private float fallDouble=1f;
+namespace Battles.Players {
+    public abstract class MoveCotroll : MonoBehaviour, IPlayerCancelProcess {
+        [SerializeField] protected float speed;
+        [SerializeField] private float jumpPower;
+        [SerializeField] private float fallDouble = 1f;
 
-		public bool IsActive{ get; set; }
-		protected Rigidbody rigid;
-		public Transform lookTarget;
+        public bool IsActive { get; set; }
+        protected Rigidbody rigid;
+        public Transform lookTarget;
 
-		protected MotionAnimControll motionAnimControll;
-		private PlayerRoot targetPlayer;
-		private IMovableAttack[] movableAttacks;
+        protected MotionAnimControll motionAnimControll;
+        private PlayerRoot targetPlayer;
+        private IMovableAttack[] movableAttacks;
 
-		public PlayerRoot TargetPlayer {
-			get {
-				if (targetPlayer == null) {
-					targetPlayer = GetComponent<IPlayerBinder>().TargetPlayerRoot;
-				}
-				return targetPlayer;
-			}
-		}
+        public PlayerRoot TargetPlayer {
+            get {
+                if (targetPlayer == null) {
+                    targetPlayer = GetComponent<IPlayerBinder>().TargetPlayerRoot;
+                }
 
-		public bool LookLock { get; set; }
+                return targetPlayer;
+            }
+        }
 
-		public float HorizontalMovement{ get; set; }
-		public float VerticalMovement{ get; set; }
-		public bool InJumping{ get; set; }
-		private bool inFall;
+        public bool LookLock { get; set; }
 
-		protected bool jumpAble = true;
-		
-		protected virtual void Start() {
-			rigid = GetComponent<Rigidbody>();
-			lookTarget = transform.Find("LookTarget");
+        public float HorizontalMovement { get; set; }
+        public float VerticalMovement { get; set; }
+        public bool InJumping { get; set; }
+        private bool inFall;
 
-			motionAnimControll = this.transform.GetComponentInChildren<MotionAnimControll>();
+        protected bool jumpAble = true;
 
-			motionAnimControll.ResponseStream
-				.Where(n=>this.IsActive)
-				.Where(n => n == AnimResponce.JumpLaunch)
-				.Subscribe(n => {
-					Jump();
-			});
-			movableAttacks = GetComponentsInChildren<IMovableAttack>();
-		}
+        protected virtual void Start() {
+            rigid = GetComponent<Rigidbody>();
+            lookTarget = transform.Find("LookTarget");
+
+            motionAnimControll = this.transform.GetComponentInChildren<MotionAnimControll>();
+
+            motionAnimControll.ResponseStream
+                .Where(n => this.IsActive)
+                .Where(n => n == AnimResponce.JumpLaunch)
+                .Subscribe(n => { Jump(); });
+            movableAttacks = GetComponentsInChildren<IMovableAttack>();
+        }
 
 
-		public abstract void Move();
+        public abstract void Move();
 
-		public virtual void AddRepulsion() {
-			var distance = Vector3.SqrMagnitude(TargetPlayer.transform.position - transform.position);
-			if (distance > 1) return;
-			rigid.AddForce(-lookTarget.forward * (400 / (distance + 1)), ForceMode.Acceleration);
-		}
-		
-		public virtual void Stop(bool force = true) {
-			if (force) {
-				if (!InJumping && jumpAble) {
-					PlayMotion(motionAnimControll.MyDic.WaitName);
-					transform.rotation = lookTarget.rotation;
-				}
-			} else {
-				if (movableAttacks.Any(i => i.IsActive)) {
-					return;
-				}
-			}
+        public virtual void AddRepulsion() {
+            var distance = Vector3.SqrMagnitude(TargetPlayer.transform.position - transform.position);
+            var centerDistance = Vector3.SqrMagnitude(new Vector3(transform.position.x, 0, transform.position.z));
+            var radius = 22;
+            if (distance <= 1) {
+                rigid.AddForce(-lookTarget.forward * (400 / (distance + 1)), ForceMode.Acceleration);
+            }
+            if (centerDistance >= radius * radius) {
+                rigid.AddForce(
+                    -new Vector3(transform.position.x, 0, transform.position.z).normalized * (centerDistance),
+                    ForceMode.Acceleration);
+            }
+        }
 
-			rigid.velocity = new Vector3(0, rigid.velocity.y, 0);
-			rigid.angularVelocity = Vector3.zero;
-		}
+        public virtual void Stop(bool force = true) {
+            if (force) {
+                if (!InJumping && jumpAble) {
+                    PlayMotion(motionAnimControll.MyDic.WaitName);
+                    transform.rotation = lookTarget.rotation;
+                }
+            } else {
+                if (movableAttacks.Any(i => i.IsActive)) {
+                    return;
+                }
+            }
 
-		public virtual void Pause(){
-			transform.rotation = lookTarget.rotation;
-			rigid.velocity = new Vector3(0, rigid.velocity.y, 0);
-			rigid.angularVelocity = Vector3.zero;
-		}
+            rigid.velocity = new Vector3(0, rigid.velocity.y, 0);
+            rigid.angularVelocity = Vector3.zero;
+        }
 
-		public virtual void JumpStart(){
-			if (InJumping||!jumpAble) return;
-			jumpAble = false;
-			var name = motionAnimControll.MyDic.JumpStartName;
-			if (name == ""){
-				Jump();
-			} else{
-				motionAnimControll.ChangeAnim(name);
-				rigid.velocity = new Vector3(0, rigid.velocity.y, 0);	
-			}
-		}
+        public virtual void Pause() {
+            transform.rotation = lookTarget.rotation;
+            rigid.velocity = new Vector3(0, rigid.velocity.y, 0);
+            rigid.angularVelocity = Vector3.zero;
+        }
 
-		protected virtual void Jump(){
-			if(InJumping)return;
-			InJumping = true;
-			rigid.AddForce(Vector3.up*jumpPower,ForceMode.Impulse);
-		}
+        public virtual void JumpStart() {
+            if (InJumping || !jumpAble) return;
+            jumpAble = false;
+            var name = motionAnimControll.MyDic.JumpStartName;
+            if (name == "") {
+                Jump();
+            } else {
+                motionAnimControll.ChangeAnim(name);
+                rigid.velocity = new Vector3(0, rigid.velocity.y, 0);
+            }
+        }
 
-		public virtual void ForceFall(){
-			rigid.velocity=new Vector3(rigid.velocity.x,0,rigid.velocity.z);
-			rigid.AddForce(Vector3.down*jumpPower*fallDouble,ForceMode.Impulse);
-		}
+        protected virtual void Jump() {
+            if (InJumping) return;
+            InJumping = true;
+            rigid.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+        }
 
-		public virtual void FallCheck(bool other_motion){
-			if(!InJumping)return;
-			if (!inFall&&rigid.velocity.y > 0.1f&&!other_motion){
-				PlayMotion(motionAnimControll.MyDic.JumpName);
-			}
-			if (!inFall&&rigid.velocity.y < -0.1f){
-				rigid.AddForce(Vector3.down*jumpPower*fallDouble,ForceMode.Impulse);
-				inFall = true;
-			}
+        public virtual void ForceFall() {
+            rigid.velocity = new Vector3(rigid.velocity.x, 0, rigid.velocity.z);
+            rigid.AddForce(Vector3.down * jumpPower * fallDouble, ForceMode.Impulse);
+        }
 
-			if (inFall && !other_motion){
-				PlayMotion(motionAnimControll.MyDic.FallName);
-			}
+        public virtual void FallCheck(bool other_motion) {
+            if (!InJumping) return;
+            if (!inFall && rigid.velocity.y > 0.1f && !other_motion) {
+                PlayMotion(motionAnimControll.MyDic.JumpName);
+            }
 
-			if (inFall && Math.Abs(rigid.velocity.y) < 0.01f){
-				inFall = false;
-				InJumping = false;
-				jumpAble = true;
-			}
-		}
+            if (!inFall && rigid.velocity.y < -0.1f) {
+                rigid.AddForce(Vector3.down * jumpPower * fallDouble, ForceMode.Impulse);
+                inFall = true;
+            }
 
-		protected void PlayMotion(string name){
-			motionAnimControll.ChangeAnim(name);
-		}
+            if (inFall && !other_motion) {
+                PlayMotion(motionAnimControll.MyDic.FallName);
+            }
 
-		public void Cancel(){
-			inFall = false;
-			InJumping = false;
-			jumpAble = true;
-			ForceFall();
-		}
-	}
+            if (inFall && Math.Abs(rigid.velocity.y) < 0.01f) {
+                inFall = false;
+                InJumping = false;
+                jumpAble = true;
+            }
+        }
+
+        protected void PlayMotion(string name) {
+            motionAnimControll.ChangeAnim(name);
+        }
+
+        public void Cancel() {
+            inFall = false;
+            InJumping = false;
+            jumpAble = true;
+            ForceFall();
+        }
+    }
 }
